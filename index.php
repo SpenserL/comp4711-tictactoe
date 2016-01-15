e7b21e6da6a59985dadb5fd896cc421258ea196a
Spenser Lee <Spenser.L159@gmail.com>
2016-01-14 23:44:23 -0800

    php strings

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>COMP 4711 Lab 1</title>
+    </head>
+    <body>
+    <?php
+
+    $temp = "Jim";
+    echo "Hi, my name is ";
+    echo $temp;
+    $temp = "geek";
+    echo "I am a";
+    echo $temp;
+    $temp = 10;
+    echo "My level is ";
+    echo $temp;
+
+    ?>
+    </body>
+</html>

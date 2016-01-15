6bb75802c26fcf8b4ecef0191f12753caf12ec78
Spenser Lee <Spenser.L159@gmail.com>
2016-01-15 04:34:41 -0800

    php objects

--- index.php
+++ index.php
@@ -5,43 +5,17 @@
     </head>
     <body>
     <?php
-
-    $name = "Jim";
-    $what = "geek";
-    $level = 10;
-    echo "Hi, my name is " . $name . " and I am a level " . $level . " " . $what;
-
-    echo "<br>";
-
-    if (isset($_GET["hours"])) {
-        $hoursworked = $_GET["hours"];
-    } else {
-        $hoursworked = 10;
-    }
-    $rate = 12;
-    if ($hoursworked > 40) {
-        $total = $hoursworked * $rate * 1.5;
-    } else {
-        $total = $hoursworked * $rate;
-    }
-
-    echo ($total > 0) ? "You owe me " . $total : "Your're Welcome";
-
     // tic tac toe
     if (isset($_GET["board"])) {
 
-        $position = $_GET["board"];
-        // check to ensure board param is valid (i.e. prevents invalid indexing)
-        if (strlen($position) < 9) {
-            $position = str_pad($position, 9, "-");
-        }
-        $squares = str_split($position);
-        echo "<br>";
+        $squares = $_GET["board"];
 
-        if (winner("x", $squares)) {
-            echo "You win.";
-        } else if (winner("o", $squares)) {
-            echo "I win.";
+        $game = new Game($squares);
+
+        if ($game->winner("x")) {
+            echo "X wins.";
+        } else if ($game->winner("o")) {
+            echo "O wins.";
         } else {
             echo "No winner yet.";
         }
@@ -52,28 +26,50 @@
 </html>
 
 <?php
-function winner($token, $position) {
-    // rows
-    for ($row = 0; $row < 3; $row++) {
-        if (($position[3*$row] == $token) && ($position[3*$row+1] == $token) && ($position[3*$row+2] == $token)) {
-            return true;
+
+class Game {
+    var $position;
+
+    function __construct($squares) {
+        if (strlen($squares) < 9) {
+            $this->position = str_pad($squares, 9, "-");
+        } else {
+            $this->position = str_split($squares);
         }
-    }
-    // cols
-    for ($col = 0; $col < 3; $col++) {
-        if (($position[$col] == $token) && ($position[$col+3] == $token) && ($position[$col+6] == $token)) {
-            return true;
-        }
-    }
-    // diag 1
-    if ($position[0] == $token && ($position[4] == $token) && ($position[8] == $token)) {
-        return true;
-    }
-    // diag 2
-    if ($position[2] == $token && ($position[4] == $token) && ($position[6] == $token)) {
-        return true;
     }
 
-    return false;
+    function winner($token) {
+        // rows
+        for ($row = 0; $row < 3; $row++) {
+            if (($this->position[3*$row]    == $token) &&
+                ($this->position[3*$row+1]  == $token) &&
+                ($this->position[3*$row+2]  == $token)) {
+                return true;
+            }
+        }
+        // cols
+        for ($col = 0; $col < 3; $col++) {
+            if (($this->position[$col]      == $token) &&
+                ($this->position[$col+3]    == $token) &&
+                ($this->position[$col+6]    == $token)) {
+                return true;
+            }
+        }
+        // diag 1
+        if (($this->position[0] == $token) &&
+            ($this->position[4] == $token) &&
+            ($this->position[8] == $token)) {
+            return true;
+        }
+        // diag 2
+        if (($this->position[2] == $token) &&
+            ($this->position[4] == $token) &&
+            ($this->position[6] == $token)) {
+            return true;
+        }
+
+        return false;
+    }
 }
+
 ?>

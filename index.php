10de54ad8806de7240147e67ce17443cbd30cf6d
Spenser Lee <Spenser.L159@gmail.com>
2016-01-15 05:13:11 -0800

    added display and comments

--- index.php
+++ index.php
@@ -5,21 +5,19 @@
     </head>
     <body>
     <?php
-    // tic tac toe
-    if (isset($_GET["board"])) {
 
-        $squares = $_GET["board"];
+    $board = (isset($_GET["board"])) ? $_GET["board"] : null;
+    $game = new Game($board);
 
-        $game = new Game($squares);
-
-        if ($game->winner("x")) {
-            echo "X wins.";
-        } else if ($game->winner("o")) {
-            echo "O wins.";
-        } else {
-            echo "No winner yet.";
-        }
+    if ($game->winner("x")) {
+        echo "X wins.";
+    } else if ($game->winner("o")) {
+        echo "O wins.";
+    } else {
+        echo "No winner yet.";
     }
+    echo "<p>Welcome to George, the evil Tic-Tac-Toe Game.</p><br>";
+    $game->display();
 
     ?>
     </body>
@@ -30,15 +28,46 @@
 class Game {
     var $position;
 
-    function __construct($squares) {
-        if (strlen($squares) < 9) {
-            $this->position = str_pad($squares, 9, "-");
-        } else {
-            $this->position = str_split($squares);
+    // construct the game object
+    public function __construct($board) {
+        // check to see if the passed in board is empty or invalid
+        // if invalid, pad with - until of length 9
+        if (empty($board) || strlen($board) < 9) {
+            $this->position = str_pad($board, 9, "-");
         }
+        // split into an array
+        $this->position = str_split($this->position);
     }
 
-    function winner($token) {
+    // display the game board
+    public function display() {
+        echo "<table cols=”3” style=”font­size:large; font­weight:bold”>";
+        echo "<tr>"; // open the first row
+        for ($pos = 0; $pos < 9; $pos++) {
+            echo $this->show_cell($pos);
+            if ($pos % 3 == 2) echo "</tr><tr>"; // start a new row for the next square
+        }
+        echo "</tr>"; // close the last row
+        echo "</table>";
+    }
+    // get the appropriate character to display on the game board, else return a hyphen
+    private function show_cell($cell) {
+        $token = $this->position[$cell];
+
+        if ($token != "-") {
+            return "<td>" . $token . "</td>";
+        }
+
+        $this->newposition = $this->position;
+        $this->newposition[$cell] = "o";
+
+        $move = implode($this->newposition);
+        $link = "/?board=" . $move;
+
+        return "<td><a href='$link'>-</a></td>";
+    }
+    // tic tac toe logic for finding a winner
+    public function winner($token) {
         // rows
         for ($row = 0; $row < 3; $row++) {
             if (($this->position[3*$row]    == $token) &&
